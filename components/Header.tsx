5cbc1838acd7a54b7bb2c39ce294386d1536de9a
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-09-12 18:32:14 -0400

    :zap: ran into errors

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex- justify-between p-5 max-w-7xl mx-auto">
+    <header className="flex flex-row justify-between p-5 max-w-7xl mx-auto">
+      <Link href="/">
+        <img
+          className="w-44 object-contain cursor-pointer"
+          src="https://links.papareact.com/yvf"
+          alt=""
+        />
+      </Link>
       <div className="flex items-center space-x-5">
-        <Link href="/">
-          <img
-            className="w-44 object-contain cursor-pointer"
-            src="https://links.papareact.com/yvf"
-            alt=""
-          />
-        </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
